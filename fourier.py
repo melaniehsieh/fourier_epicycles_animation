9c8f71bd9ff1c9129c1156d4df799a70d1b0fd13
Melanie Hsieh <mpwh2004@gmail.com>
2021-02-15 11:07:43 +0800

    sin cos tan animation

--- fourier.py
+++ fourier.py
@@ -3,6 +3,7 @@ import math
 from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt 
 
 class CircleWave:
     def __init__(self, center, wave, point_on_wave = 0, parent = None):
@@ -53,6 +54,32 @@ class SineWave:
         y = [self.func(x, t) for x in x_values]
         ax.plot(x_values, y, '-', color = self.color)
 
+class CosineWave:
+    def __init__(self, amp, omega, velocity, color):
+        self.amp = amp
+        self.omg = omega
+        self.c = velocity
+        self.color = color
+
+        self.func = lambda x, t: self.amp*math.cos(self.omg*(x - self.c*t))
+        
+    def plot_wave(self, ax, x_values, t):
+        y = [self.func(x, t) for x in x_values]
+        ax.plot(x_values, y, '-', color = self.color)
+
+class TanWave:
+    def __init__(self, amp, omega, velocity, color):
+        self.amp = amp
+        self.omg = omega
+        self.c = velocity
+        self.color = color
+
+        self.func = lambda x, t: self.amp*math.tan(self.omg*(x - self.c*t))
+        
+    def plot_wave(self, ax, x_values, t):
+        y = [self.func(x, t) for x in x_values]
+        ax.plot(x_values, y, '-', color = self.color)
+
 class FourierSeriesWave:
     def __init__(self, waves, bigcircle_x = -10, point_on_wave = 0):
         self.waves = sorted(waves, key = lambda x: x.amp, reverse=True)  
@@ -86,15 +113,17 @@ time_points = [0 + 500*i/100000 for i in range(100001)]
 n_time = len(time_points)
 
 #multiple waves
-wave = SineWave(3, 5, 5, color = (0,0,1,0.3))
-wave_2 = SineWave(3, 5.5, 11, color = (0,1,0,0.3))
-waves = [wave, wave_2]
+wave = SineWave(2, 8, 7, color = (0,0,1,0.3))
+wave_2 = CosineWave(1, 2, 10, color = (0,1,0,0.3))
+wave_3 = TanWave(3, 2, 1, color = (1,0,0,0.3))
+wave_4 = SineWave(0.5, 3, 7.5, color = (0,1,1,0.3))
+waves = [wave, wave_2, wave_3]
 
 # square wave
-# waves = [SineWave(1.5*(10/((i)*math.pi)), i, 15, color = (0,0,1,0.2)) for i in range(1, 10, 2)]
+# waves = [CosineWave(1.2*(10/((i)*math.pi)), i, 15, color = (0,0,1,0.2)) for i in range(1, 10, 2)]
 
 # sawtooth wave
-# waves = [SineWave(3*(3/((i)*math.pi)), i, 20, color = (0,1,0,0.3)) for i in range(1, 5)]
+# waves = [CosineWave(3*(3/((i)*math.pi)), i, 20, color = (0,1,0,0.3)) for i in range(1, 9)]
 
 x = [0 + 10*math.pi*i/5000 for i in range(5001)]
 
@@ -114,12 +143,14 @@ def animate(frame):
     draw_points_y.append(point[1])
 
     ax.plot(draw_points_x, draw_points_y, '-', color = (0,0,1,0.8))
-    equation_title = "3sin(x - 20t) + 2sin(3x - 6t)"
+    equation_title = "2sin(8x - 7t) + cos(2x - 10t) + 3tan(2x - t) + 0.5sin(3x - 7.5t)"
     ax.set_title(equation_title, color = 'black')
 
     ax.set_xlim([-20, 20])
     ax.set_ylim([-10, 10])
     
+plt.pause(3)
 animation = FuncAnimation(fig, animate, frames = range(n_time), repeat = False, interval = 50)
+
 plt.show()
 
